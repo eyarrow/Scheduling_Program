de44850fe3ddd76936e3dbaaacf960eaa1b7c5b4
Elizabeth Yarrow <eyarrow@outlook.com>
2021-09-24 18:49:30 -0700

    Login form design updated.

--- src/scheduler/controller/LoginController.java
+++ src/scheduler/controller/LoginController.java
@@ -1,4 +1,23 @@
 package scheduler.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField textUserID;
+
+    @FXML
+    private Button buttonSignIn;
+
+    @FXML
+    private PasswordField textPassword;
+
+    @FXML
+    private Label labelZoneID;
+
 }
