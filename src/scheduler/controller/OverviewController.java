f3744b26fe1249833d7eeb13673050815c34679a
Elizabeth Yarrow <eyarrow@outlook.com>
2021-09-25 14:52:24 -0700

    Overview UI commit

---
+++ src/scheduler/controller/OverviewController.java
@@ -0,0 +1,33 @@
+package scheduler.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class OverviewController {
+    @FXML
+    private Button buttonOverview;
+
+    @FXML
+    private Button buttonCustomers;
+
+    @FXML
+    private Button buttonAppointments;
+
+    @FXML
+    private Button buttonReports;
+
+    @FXML
+    private Button buttonLogout;
+
+    @FXML
+    private Label labelApptToday;
+
+    @FXML
+    private Label labelAppointTomorrow;
+
+    @FXML
+    private Label labelAppointThisWeek;
+
+}
