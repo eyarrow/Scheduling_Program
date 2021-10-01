4e6aa93c306f5bb52b789dceb7f3fd9fc00f19dc
Elizabeth Yarrow <eyarrow@outlook.com>
2021-10-01 16:22:16 -0400

    DB Driver configured to 8.0.25. Connection with database is working in testing.

--- src/scheduler/Main.java
+++ src/scheduler/Main.java
@@ -5,19 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import scheduler.Dao.dbOperations;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Acme Consulting : Login ");
+        primaryStage.setScene(new Scene(root, 600, 552));
         primaryStage.show();
+        dbOperations.openConnection();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+        dbOperations.closeConnection();
     }
 }
