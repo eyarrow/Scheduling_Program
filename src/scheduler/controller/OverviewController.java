0868ae0bc8b40e77c7b9edfa6208a887ac9f4937
Elizabeth Yarrow <eyarrow@outlook.com>
2021-10-01 21:18:24 -0400

    Login validation and connection tested. The application can successfully determine whether a user has a valid login or not, and is able to display the appropriate error message.

--- src/scheduler/controller/OverviewController.java
+++ src/scheduler/controller/OverviewController.java
@@ -1,11 +1,15 @@
 package scheduler.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class OverviewController {
+
+public class OverviewController implements Initializable {
     @FXML
     private Button buttonOverview;
 
@@ -29,5 +33,15 @@ public class OverviewController {
 
     @FXML
     private Label labelAppointThisWeek;
+
+    /**
+     * Initializes the Overview Controller
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
 }
