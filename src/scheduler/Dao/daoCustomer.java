677a50e87d278a7ea1d68ca3c94fba022243440c
Elizabeth Yarrow <eyarrow@outlook.com>
2021-10-03 15:42:31 -0400

    Setting up Observable List for Customer

--- src/scheduler/Dao/daoCustomer.java
+++ src/scheduler/Dao/daoCustomer.java
@@ -1,10 +1,20 @@
 package scheduler.Dao;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import scheduler.model.Customer;
+
 /**
  * Class that provides functionality on the persistence layer for the "Customer" class that manages business local in the model package.
  *
  */
 public abstract class daoCustomer {
 
+    public static ObservableList<Customer> getAllCustomers() {
+        ObservableList<Customer> cList = FXCollections.observableArrayList();
+
+
+        return cList;
+    }
 
 }
